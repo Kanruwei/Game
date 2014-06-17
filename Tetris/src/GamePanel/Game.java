b7b9e055d671ebb9f9f2c8e17b3c769f3164ea44
Kanruwei <kanruwei@gmail.com>
2014-06-17 22:12:20 +0200

    example images of games have been uploaded.

--- Tetris/src/GamePanel/Game.java
+++ Tetris/src/GamePanel/Game.java
@@ -12,7 +12,7 @@ public class Game extends JFrame{
 	
 	public static void main(String args[]){
 		
-		JFrame Frame = new JFrame("Tetric");
+		JFrame Frame = new JFrame("Tetric V1.0");
 		Frame.setMaximumSize(new Dimension(WIDTH, HEIGHT));
 		Frame.setMinimumSize(new Dimension(WIDTH, HEIGHT));
 		Frame.setPreferredSize(new Dimension(WIDTH, HEIGHT));
