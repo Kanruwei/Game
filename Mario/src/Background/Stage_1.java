43c92dd7a179c6933d04793bc6087d696676c3eb
Kanruwei <kanruwei@gmail.com>
2014-06-18 17:05:05 +0200

    little update...

--- Mario/src/Background/Stage_1.java
+++ Mario/src/Background/Stage_1.java
@@ -7,8 +7,10 @@ import Enemy.Npg;
 public class Stage_1 extends Map{
 	
 	public Point location_1 = new Point(448, 608);
+	public Point location_2 = new Point(256, 736);
 
 	public Npg npg_1;
+	public Npg npg_2;
 	
 	public Stage_1() {
 		super.path_image = "res/Backgrounds/Stage_1.png";
@@ -16,7 +18,9 @@ public class Stage_1 extends Map{
 		super.path_bricks = "res/Backgrounds/Brick.png";
 		
 		super.Location_Npg.add(location_1);
+		super.Location_Npg.add(location_2);
 		
 		npg_1 = new Npg(this, super.gruppe);
+		npg_2 = new Npg(this, super.gruppe);
 	}	
 }
--- Mario/src/Enemy/Npg.java
+++ Mario/src/Enemy/Npg.java
@@ -14,6 +14,8 @@ public class Npg {
 	public String path_image = "res/Sprites/frog.png";
 	public Image image;
 	
+	public int speed = 4;
+	
 	public int X;
 	public int Y;
 	
@@ -57,5 +59,6 @@ public class Npg {
 	
 	public void update(){
 		
+		patrol();
 	}
 }
